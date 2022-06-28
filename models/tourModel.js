2906a48b4fff2a512f2e4fe9afb13314021bc998
Łukasz Dawidowicz <lddev@outlook.com>
2022-06-28 22:06:52 +0200

    add data import js

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,14 +6,43 @@ const tourSchema = new mongoose.Schema({
     required: [true, "Name is requierd"],
     unique: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    requierd: [true, "Need duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    requierd: [true, "Nedd max group number"],
+  },
+  difficulty: {
+    type: String,
+    requierd: [true, "requierd"],
+  },
+  ratingAverage: {
     type: Number,
     default: 3.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     requierd: [true, "Price is requierd"],
   },
+  discount: Number,
+  summary: {
+    type: String,
+    trim: true,
+  },
+  description: { type: String, trim: true },
+  imageCover: { type: String, requierd: [true, "need img"] },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
