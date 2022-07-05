8a087e5944e977ac6bde0f12acd8dc90a137e9f3
Łukasz Dawidowicz <lddev@outlook.com>
2022-07-05 22:34:23 +0200

    add virtual field

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,49 +1,59 @@
 const mongoose = require("mongoose");
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is requierd"],
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is requierd"],
+      unique: true,
+    },
+    duration: {
+      type: Number,
+      requierd: [true, "Need duration"],
+    },
+    maxGroupSize: {
+      type: Number,
+      requierd: [true, "Nedd max group number"],
+    },
+    difficulty: {
+      type: String,
+      requierd: [true, "requierd"],
+    },
+    ratingAverage: {
+      type: Number,
+      default: 3.5,
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      requierd: [true, "Price is requierd"],
+    },
+    discount: Number,
+    summary: {
+      type: String,
+      trim: true,
+    },
+    description: { type: String, trim: true },
+    imageCover: { type: String, requierd: [true, "need img"] },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    requierd: [true, "Need duration"],
-  },
-  maxGroupSize: {
-    type: Number,
-    requierd: [true, "Nedd max group number"],
-  },
-  difficulty: {
-    type: String,
-    requierd: [true, "requierd"],
-  },
-  ratingAverage: {
-    type: Number,
-    default: 3.5,
-  },
-  ratingQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    requierd: [true, "Price is requierd"],
-  },
-  discount: Number,
-  summary: {
-    type: String,
-    trim: true,
-  },
-  description: { type: String, trim: true },
-  imageCover: { type: String, requierd: [true, "need img"] },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual("durationWeeks").get(function () {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
